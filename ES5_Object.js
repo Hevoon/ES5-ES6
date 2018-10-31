36d1d640878b6f4002be9f02650dd0c60c8f4d6c
LI <2943753234@qq.com>
2018-10-31 20:36:41 +0800

    i have finish the object js

--- ES5_Object.js
+++ ES5_Object.js
@@ -73,6 +73,7 @@ let detail = Object.getOwnPropertyDescriptor(persion, "school");
 console.log(detail.value);
 
 /****************************************************************************************************************/
+
 //构造函数第一个字母大写
 function Persion(name, age, job) {
     this.name = name;
@@ -85,7 +86,7 @@ function Persion(name, age, job) {
 Persion.prototype.jobs = "wroker";
 Persion.prototype.sayjob = function () {
     console.log(this.job);
-};//在构造函数的原型上添加属性和方法
+};//在构造函数的原型上添加属性和方法，原型的constructor属性指向构造函数，构造函数以及实例的原型指向Presion.prototype，实例的__proto__指向原型Presion.prototype
 let p1 = new Persion("lilili", 20, "bosser");
 let p2 = new Persion("hihihi", 20, "boss");
 console.log(p1);
@@ -99,9 +100,83 @@ console.log(p1.hasOwnProperty("name") + "-------" + p1.hasOwnProperty("jobs"));
 
 /**********************************************************************************************************/
 //in操作符与原型，in单独使用可以判断是否能访问给定对象。for-in连用会返回所有能够通过对象访问的，可枚举属性，包括实例与原型中的全部，屏蔽了原型中不可枚举属性的实例属性会被循环访问到。
-p1.jobs="what";
-for(let prop in p1){
-       console.log(prop+"----");
+p1.jobs = "what";
+for (let prop in p1) {
+    console.log(prop + "----");
 }//原型中的construction方法不可枚举，如果实例创造一个construction方法，便可以被for in枚举出来。
 console.log(Object.keys(p1));//获得所有可枚举数属性
 console.log(Object.getOwnPropertyNames(Persion.prototype));//所有属性，无论是否可枚举
+
+function Iss() {
+}
+
+Iss.prototype = {
+    constructor: Iss,
+    key: 1235,
+    username: "Hevoon",
+};//这样设置prototype会导致constructor指向Object，必须显示的设置constructor的指向，但这样做会导致constructor的enumberable属性为true从而可以枚举
+//生成实例后不要重写原型对象，这样会导致实例指向原型的指针指向原来的原型而不会改变，从而导致搜索不到方法，如下是一个重写原型的例子
+let i1 = new Iss();//此时i1中的指针已经指向了第一个ISS.prototype
+console.log(i1.key);
+Iss.prototype.hello = "hello";
+console.log(i1.hello);
+Iss.prototype = {
+    constructor: Iss,
+    key: 123587878,
+    username: "Hevoon",
+};//此语法即为重写原型对象
+let i2 = new Iss();//此时i2中的指针已经指向了第二个ISS.prototype
+console.log(i1.key);//始终指向第一个原型
+console.log(i2.key);
+console.log(i2.hello);//没有定义
+/**********************************************************************************************************/
+//闭包
+var wind = "lihong";
+let ob = {
+        wind: "yuyuyu",
+        getName: function () {
+            return function () {
+                return this.wind;
+            };
+        },
+        getthiswind: function () {
+            let that = this;
+            return function () {
+                return that.wind;
+            }
+        }
+    }
+;
+console.log(ob.getName()() + "<----这里是匿名函数的this指向,that的指向------>" +ob.getthiswind()());
+//ES6的块级作用域会在这里做出比较,闭包会导致内存泄漏，即一个闭包中如果保存一个html元素，闭包中有另一个小函数调用了这个元素的某个属性，则因为函数的1原因该元素永远不可能销毁，所以需要手动来操作。
+(function () {
+    for (let i=1;i<10;i++){
+        console.log(i);
+    }
+})();
+// console.log(i);//i未定义
+//ES5中创建块级作用域的方法，函数执行完毕没有
+function Mock_arr(){
+    this.name=1;
+    this.age=2;
+}
+let application=function () {
+    let arr=[];
+    arr.push(new Mock_arr());//假装有一个数据
+    return{
+        getarr:function () {
+            return arr.length;
+        },//返回数组数量
+        registerarr:function (arrss) {
+            if(typeof arrss ==="object"){
+                arr.push(arrss);
+            }
+        }//注册数组
+    }
+};
+let pp1=application();
+let pp2=application();
+pp1.registerarr({key:22});
+console.log(pp1.getarr()+"------"+pp2.getarr());
+console.log(pp1===pp2,pp1,pp2);
+//模块模式，创建单例，同时为单例创建私有属性和方法，单例指一个只有实例的对象（即以对象字面量来创建的对象，没有原型）。
